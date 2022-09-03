ded1fc05a3e09eec178bc283efb71759fd15a61e
Rafael De Luca <deluca1712@gmail.com>
2022-09-03 13:56:07 -0300

    Criando projeto e adicionando dependencias

---
+++ src/test/java/rafaeldeluca/com/crudclient/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package rafaeldeluca.com.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
